2b70cfbd88bdf4aeac83390208115c6eaacc253e
bmo <kevin.illu@transmares.gt>
2024-12-13 17:06:17 -0600

    feat(where raw): ahora esta listo el metodo pero le falta limpiarlo un poco

--- main.ts
+++ main.ts
@@ -1,40 +1,72 @@
 
+type queryFunction = ($: any, p: string, v: any) => any
 
 type Filters = {
-  [key: string]: ($: any, p: string, v: any) => any
+  [key: string]: queryFunction
 }
 
 type httpParams = { [key: string]: any };
 
+type MapedFilters = {
+  // columnas sin prefijos que sirven como identificadores
+  // para saber si el parametro existe en los filtros
+  identifiers: (string | undefined)[],
 
+  query: queryFunction
+
+  // columnas con prefijos
+  queryIdentifiers: (string)[]
+}[]
+
+function mapOfFilters(filters: Filters): MapedFilters {
+  return Object.entries(filters).map(([f, query]) => ({
+    identifiers: f.trim().replace(/\s+/g, '').split(',').map((q) => q.split('.')[1]),
+    queryIdentifiers: f.trim().replaceAll(/\s+/g, '').split(','),
+    query,
+  }));
+}
 
 function whereBuilder(filters: Filters) {
-  const filtersToCheck = Object.entries(filters);
-  const getArrayParams = (filterParameter: httpParams): string[] => 
-    filterParameter.trim().replaceAll(' ', '').split(',')
-
-  const getPrefixAndColumn = (param: string) => param.split('.')
-
+  const filtersStructure = mapOfFilters(filters);
   return function (queryBuilder: any, params: httpParams) {
-    for (const [filters, value] of filtersToCheck) {
-
+    const returnedFunctions = [];
+    for (const [param, value] of Object.entries(params)) {
+      for (const filter of filtersStructure) {
+        if (!filter.identifiers.includes(param)) continue;
+        const parameter = filter.queryIdentifiers[filter.identifiers.indexOf(param)];
+        returnedFunctions.push(filter.query(null, parameter!, value))
+      }
     }
+    return returnedFunctions;
   }
 }
 
 (() => {
   const httpParams = {
     cod_servicio: 89,
+    cod_oportunidad: 21,
+    cod_medio_transporte: 321,
     numero_oportunidad: [87821, 45201],
     serie_oportunidad: 12
   };
 
-  const whereCotizacion = whereBuilder({
+  const filtrosA: Filters = {
     's.cod_servicio, s.cod_oportunidad, c.cod_medio_transporte': ($, p, v) => $.where(p, v),
     's.serie_oportunidad': ($, _, v) => $.whereIn('op.serie', v),
     's.numero_oportunidad': ($, _, v) => $.whereRaw(`"op"."numero"::TEXT ILIKE '%${v}%'`)
-  });
+  }
 
+  const filtrosB: Filters  = {
+    [`
+      s.cod_servicio,
+      s.cod_oportunidad,
+      c.cod_medio_transporte
+    `]: (_, p, v) => `${p} = ${v}`,
+    's.serie_oportunidad': (__, _, v) => `op.serie = ${v}`,
+    's.numero_oportunidad': (__, _, v) => `"op"."numero"::TEXT ILIKE '%${v}%'`
+  };
 
-  whereCotizacion(null, httpParams);
+  const whereCotizacion = whereBuilder(filtrosB);
+  const result = whereCotizacion(null, httpParams);
+  console.log(result);
 })()
